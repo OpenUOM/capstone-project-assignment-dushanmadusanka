14b843b5ea8ce68350295d26552ac898c66be4aa
dushanmadusanka <dushanmadusanka485@gmail.com>
2023-01-21 01:24:13 -0800

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -135,7 +135,7 @@ const addStudent = async (id, name, age, religion) => {
 }
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM student`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
