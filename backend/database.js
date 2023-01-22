0fe22c1ec0f7e83ca636f3517547198473f49f88
dushanmadusanka <dushanmadusanka485@gmail.com>
2023-01-22 01:40:31 -0800

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -79,7 +79,7 @@ const updateTeacher = async (name, age, id) => {
 }
 
 const deleteTeacher = async (id) => {
-    const sql = `DELETE FROME teacher WHERE id = ?`
+    const sql = `DELETE * FROME teacher WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
